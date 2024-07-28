e334f7e8f9709a0697bb5d488db98ba337b7ded2
Ronaldyika <buhnyuyronald@gmail.com>
2024-07-28 13:16:51 -0700

    composable states and recomposables

---
+++ Utube_Course/app/src/main/java/com/example/beginer/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.example.beginer.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFE3D4F4)
+val Purple500 = Color(0xFF121213)
+val Purple700 = Color(0x496D6482)
+val Teal200 = Color(0xFF4CCD95)
